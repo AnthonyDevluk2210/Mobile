a81d8d13747edfaeee1cf38e8957351d6baabf63
AnthonyDevluk2210 <anthonydepaulacolliaso@gmail.com>
2025-02-25 13:05:05 -0300

    first commit

---
+++ IntroducaoPOO/OlaMundo/src/OlaMundo.java
@@ -0,0 +1,10 @@
+public class OlaMundo {
+    //atributros - caracteristicas
+    String nomeUsuario;
+
+
+    //métodos - ações
+    public void print(){
+        System.out.println("Olá, Mundo!!!");
+    }
+}
