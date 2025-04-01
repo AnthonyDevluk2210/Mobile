34874198afe0d1de2dd311c069ba5ccd8d1c4ab2
AnthonyDevluk2210 <anthonydepaulacolliaso@gmail.com>
2025-04-01 15:02:01 -0300

    01.04.25

--- IntroducaoFlutter/exercicio_layout/lib/main.dart
+++ IntroducaoFlutter/exercicio_layout/lib/main.dart
@@ -1,43 +1,129 @@
+// tela de perfil com flutter
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: MyApp(), debugShowCheckedModeBanner: false));
+  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context){
     return Scaffold(
-      appBar: AppBar(title: Text("Atividade Pratica")),
-      body: Center(
+      appBar: AppBar(title: Text("Perfil do Usuário"),
+      actions: [
+        IconButton(
+          icon: Icon(Icons.settings),
+          onPressed: () => print("Configurações Pressionado"),
+        ),
+        IconButton(
+          onPressed: () => print("Sair Pressionado"), 
+          icon: Icon(Icons.exit_to_app))
+      ],
+      backgroundColor: Colors.grey,
+      
+      ),
+      // imagem do usuario e informações do perfil
+      body: Padding(
+        padding: EdgeInsets.all(8),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
           children: [
-            Stack(
-              alignment: Alignment.topCenter,
+            // informações do perfil
+            Container(
+              width: double.infinity,
+              color: Colors.grey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ClipOval(
+                    child: Image.network(
+                      "https://ducktales.fandom.com/pt-br/wiki/Pato_Donald",
+                      width: 200,
+                      height: 200,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  Text("Nome: Pato Donald", style: TextStyle(fontSize: 20)),
+                  Text("dataNascimento: 12/06/1985", style: TextStyle(fontSize: 20)),
+                ],
+              ),
+            ),
+            
+            // 3 container para exibir informações
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Container(
-                  color: Colors.grey,
-                  height: 100,
-                  width: 100,
+                  margin: EdgeInsets.all(10),
+                  padding: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    color: const Color.fromARGB(255, 243, 33, 33),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Text("Seguidores: 20", style: TextStyle(fontSize: 20, color: Colors.white)),
                 ),
-                  Image.asset("")
+                Container(
+                  margin: EdgeInsets.all(10),
+                  padding: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    color: const Color.fromARGB(255, 145, 243, 33),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Text("Postagens: 110", style: TextStyle(fontSize: 20, color: Colors.white)),
+                ),
+                Container(
+                  margin: EdgeInsets.all(10),
+                  padding: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    color: const Color.fromARGB(255, 243, 33, 33),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Text("Amigos proximos: 10", style: TextStyle(fontSize: 20, color: Colors.white)),
+                ),
               ],
             ),
-            Text("Nome: Leandro.",
-            style: TextStyle(
-              fontSize: 18,
-              color: Colors.tealAccent,
-              )),
-            Text("Idade: 20",
-            textAlign: TextAlign.left,
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.normal,
-              color: Colors.tealAccent,
-            ),),
+
+            //lista com icones de redes sociais
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              
+              children: [
+                IconButton(
+                  onPressed: () => print("Facebook Pressionado"),
+                  icon: Icon(Icons.facebook),
+                  iconSize: 50,
+                ),
+                IconButton(
+                  onPressed: () => print("Instagram Pressionado"),
+                  icon: Icon(Icons.camera_alt),
+                  iconSize: 50,
+                ),
+                IconButton(
+                  onPressed: () => print("Twitter Pressionado"),
+                  icon: Icon(Icons.alternate_email),
+                  iconSize: 50,
+                ),
+              ],
+            ),
           ],
         ),
+      ) ,
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: "Buscar",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add),
+            label: "Adicionar",
+          ),
+        ],
       ),
     );
   }
