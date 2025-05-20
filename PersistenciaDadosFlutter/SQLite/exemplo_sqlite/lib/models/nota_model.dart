632d0cbfbfc5ab2daa908f85af4e357a018d805b
AnthonyDevluk2210 <anthonydepaulacolliaso@gmail.com>
2025-05-20 16:41:25 -0300

    20.05.2025

--- PersistenciaDadosFlutter/SQLite/exemplo_sqlite/lib/models/nota_model.dart
+++ PersistenciaDadosFlutter/SQLite/exemplo_sqlite/lib/models/nota_model.dart
@@ -17,12 +17,22 @@ class Nota {
   //converter dados parao banco de dados
   // método MAP => converte um objeto da classe Nota para uma Map(para inserir no Banco de Dados)
   Map<String, dynamic> toMap() {
-    return {
-      "id" : id,
-      "titulo" : titulo,
-      "conteudo" : conteudo,
-    };
+    return {"id": id, "titulo": titulo, "conteudo": conteudo};
   }
 
-  // factory
+  // factory -> converte dados do BD para um Obj
+  factory Nota.fromMap(Map<String, dynamic> map) {
+    return Nota(
+      id: map["id"] as int, //cast
+      titulo: map["titulo"] as String,
+      conteudo: map["conteudo"] as String,
+    );
+  }
+
+  //toString
+  @override
+  String toString() {
+    // TODO: implement toString
+    return "Nota{id: $id, Título: $titulo, Conteúdo: $conteudo}";
+  }
 }
